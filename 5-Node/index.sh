cb5f6eb3f3f8e4c73332ad2e8a2588a63e16ec12
Muhammad Jidan <muhammadjidan703@gmail.com>
2025-11-24 00:11:53 +0700

    learn kubernetes

---
+++ 5-Node/index.sh
@@ -0,0 +1,6 @@
+# untuk melihat semua node di cluster Kubernetes
+kubectl get nodes
+
+# untuk melihat detail dari node tertentu
+# kubectl describe node <nama-node>
+kubectl describe node docker-desktop
---
+++ 6-Pod/index.sh
@@ -0,0 +1,6 @@
+# untuk mendapatkan daftar pod di Kubernetes
+kubectl get pod
+
+# untuk mendapatkan informasi detail tentang sebuah pod tertentu
+# kubectl describe pod <nama-pod>
+kubectl describe pod namapod
